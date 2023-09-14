2cf21c6b654fa326ab86e89b9aeb40001ca8ba8e
Maryia <asha.87@list.ru>
2023-09-14 22:42:57 +0300

    добавила задачу про нахождение четных чисел в массиве

---
+++ test34/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте массив заполненный случайными
+//положительными трёхзначными числами. Напишите
+//программу, которая покажет количество чётных чисел в
+//ссиве.
+
+int InputNum (string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);  
+}
+
+
+void FillArray(int[] array)
+{
+    Random rnd= new Random();
+    for (int i = 0; i < array.Length; i++)
+        array[i] = rnd.Next(100, 999);  
+}
+
+int FindNum(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        count ++;
+    }
+    return count;   
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine(array[i]);
+    }
+}
+
+int size = InputNum("Введите размер массива: ");
+int[] array = new int [size];
+FillArray(array);
+PrintArray(array);
+int count = FindNum(array);
+Console.WriteLine($"Количество четных чисел в массиве = {count}");
+      
