b7cc2f13113eded1cb0e45c1c84282bcea7f1339
Maryia <asha.87@list.ru>
2023-09-14 23:05:57 +0300

    добавила задачу про сумму элементов массива с нечетными инждексами

---
+++ test36/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задайте одномерный массив, заполненный
+//случайными числами. Найдите сумму элементов, стоящих
+//на нечётных позиция
+
+int InputNum (string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);  
+}
+
+
+void FillArray(int[] array)
+{
+    Random rnd= new Random();
+    for (int i = 0; i < array.Length; i++)
+        array[i] = rnd.Next(1, 100);  
+}
+
+int FindSum(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i+=2)
+    {
+        sum = sum + array[i];
+    }
+    return sum;   
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+}
+
+int size = InputNum("Введите размер массива: ");
+int[] array = new int [size];
+FillArray(array);
+PrintArray(array);
+int sum = FindSum(array);
+Console.WriteLine($"сумма элемнтов с нечетными индексами = {sum}");
+      
